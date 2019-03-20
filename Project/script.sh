6bc1c4e5b915345366885d1749bd0426c84cb4ec
digiblackdranzer <ourgour@gmail.com>
2019-03-20 23:02:13 +0530

    Utility for Algorand

--- Project/script.sh
+++ Project/script.sh
@@ -1,1 +1,2 @@
 export PATH=$PATH:/usr/bin/openssl
+sudo pip3 install fastecdsa # Refer https://github.com/AntonKueltz/fastecdsa
---
+++ Project/utils.py
@@ -0,0 +1,35 @@
+from fastecdsa import keys,curve,ecdsa
+from hashlib import sha384
+import random
+import numpy as np
+
+def generateKeys():
+    priv_key,pub_key = keys.gen_keypair(curve.P256) # Refer https://safecurves.cr.yp.to/ Options : P256,P224,...
+    return [priv_key,pub_key]
+
+def generateNumberOfNeighbors():
+    return random.choice(list(range(4,9)))
+
+def generateBlockDelay():
+    return max(0,np.random.normal(200,400))
+
+def generateNonBlockDelay():
+    return max(0,np.random.normal(30,64))
+
+def signMessage(message,priv_key):
+    r,s = ecdsa.sign(message,priv_key)
+    return [r,s]
+
+def verifySignature(r,s,message,pub_key):
+    return ecdsa.verify((r,s),message,pub_key)
+
+def assignStake():
+    return random.choice(list(range(1,51)))
+
+# For testing Purpose Only
+
+if __name__ == '__main__' :
+    print(generateKeys())  # Private keys in ECDSA are integers while Public Key are points on curve
+    print(generateNumberOfNeighbors())
+
+
